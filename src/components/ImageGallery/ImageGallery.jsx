12f6538f1fb34705efe0dcf91d4f06971401386a
Yuliia-Zinchuk <yuliya_zinchuk@icloud.com>
2022-10-18 19:25:46 +0300

    update components ImageGallery and ImageGalleryItem

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,16 +1,25 @@
 import PropTypes from 'prop-types';
 import css from './ImageGallery.module.css';
-import { ImageGalleryItem } from './ImageGalleryItem';
+import { ImageGalleryItem } from '../ImageGalleryItem/ImageGalleryItem';
 
 export const ImageGallery = ({ images, openModal }) => {
   return (
-    <ul className={css.ImageGallery}>
-      <ImageGalleryItem images={images} openModal={openModal} />
-    </ul>
+    <>
+      <ul className={css.ImageGallery}>
+        {images.map(({ id, largeImageURL, webformatURL }) => (
+          <ImageGalleryItem
+            key={id}
+            largeImageURL={largeImageURL}
+            webformatURL={webformatURL}
+            openModal={openModal}
+          />
+        ))}
+      </ul>
+    </>
   );
 };
 
 ImageGallery.propTypes = {
-  openModal: PropTypes.func.isRequired,
   images: PropTypes.array.isRequired,
+  openModal: PropTypes.func.isRequired,
 };
